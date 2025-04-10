96f97e96d1943f5246022ccc9cc198c11975bf8f
Eng.Mokhtar Alahdal <58419254+alahdalmokhtar@users.noreply.github.com>
2025-04-11 00:18:30 +0200

    Quetions 1 and 2 is done .

---
+++ groep_team2_week1.py
@@ -0,0 +1,11 @@
+#Qustion num 1 : 
+for i in range(1,11):
+    print(i)
+
+#====================
+#Question num 2:
+x=int(input("enter the number : "))
+even=[]
+for i in range (x):
+    if i % 2 ==0:
+        print(i)
